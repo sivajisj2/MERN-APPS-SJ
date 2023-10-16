10c279cceaeb1e236ce9de5d000138627a8a1080
sivajisj2 <sjsjsjsivaji@gmail.com>
2023-10-16 07:45:43 +0000

    login page is being implemented

--- FullStack-Blog/client/src/App.jsx
+++ FullStack-Blog/client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
 import Login from './components/account/Login'
+import BoxSx from './components/account/BoxSx'
 const App = () => {
   return (
     <>
     <Login/>
+
     </>
   )
 }
--- FullStack-Blog/client/src/components/account/Login.jsx
+++ FullStack-Blog/client/src/components/account/Login.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { TextField, Box, Button, Typography, styled } from '@mui/material';
+const imageURL = 'sjlogo2.png';
+const Component = styled(Box)`
+    width: 400px;
+    margin: auto;
+    box-shadow: 5px 2px 5px 2px rgb(0 0 0/ 0.6);
+    `;
 
+    const Image = styled('img')`
+    width:200
+   
+    `;
 const Login = ()=>{
-    return <h1>Hello From Login</h1>
+   
+    
+
+    return (
+         <Component>
+            <Box>
+            <Image  className="logo" src={imageURL} alt="Login"/>
+                <Box>
+                  <TextField variant="standard"/>
+                  <TextField variant="standard"/>
+                    <Button variant="contained">Login</Button>
+                   <Button>Create an account</Button>
+           
+                </Box>
+            </Box>
+           
+         
+         
+          
+           
+            
+         </Component>
+        )
 }
 
 export default Login
